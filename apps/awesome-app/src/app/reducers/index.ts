04314c1b8bd50b78fd34e7d99526d2ed63db919f
vivek kumar <vivekkumar27june88@gmail.com>
2019-11-25 17:13:18 +0530

    NgRx Meta Reducer'

--- apps/awesome-app/src/app/reducers/index.ts
+++ apps/awesome-app/src/app/reducers/index.ts
@@ -1,5 +1,5 @@
 import { routerReducer } from '@ngrx/router-store';
-import { ActionReducerMap, MetaReducer } from '@ngrx/store';
+import { ActionReducerMap, MetaReducer, ActionReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 
 export interface AppState {}
@@ -8,6 +8,19 @@ export const reducers: ActionReducerMap<AppState> = {
   router: routerReducer
 };
 
+export function customLoggerMetaReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return (state, action) => {
+    console.log({
+      state,
+      action
+    });
+
+    return reducer(state, action);
+  };
+}
+
 export const metaReducers: MetaReducer<AppState>[] = !environment.production
-  ? []
+  ? [customLoggerMetaReducer]
   : [];
