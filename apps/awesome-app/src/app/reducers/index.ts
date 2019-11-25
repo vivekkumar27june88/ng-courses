0f359f907093c31075c2439593df076632b26a79
vivek kumar <vivekkumar27june88@gmail.com>
2019-11-25 16:23:43 +0530

    Router State Configuration

--- apps/awesome-app/src/app/reducers/index.ts
+++ apps/awesome-app/src/app/reducers/index.ts
@@ -1,9 +1,12 @@
+import { routerReducer } from '@ngrx/router-store';
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 
 export interface AppState {}
 
-export const reducers: ActionReducerMap<AppState> = {};
+export const reducers: ActionReducerMap<AppState> = {
+  router: routerReducer
+};
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production
   ? []
